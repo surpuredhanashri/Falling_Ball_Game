c703604d67b3fb95a1e0e44a851f952c84173fe0
surpuredhanashri <surpuredhanashri@gmail.com>
2020-10-11 09:11:57 +0530

    third commit

--- script.js
+++ script.js
@@ -5,66 +5,86 @@ var both = 0;
 var counter = 0;
 var currentBlocks = [];
 
-function moveLeft(){            //move ball left side
+//create a variable equal to the current left position of the character, and then set a new left position for the 
+// character subtracting 2 pixels. 
+function moveLeft(){           
     var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     if(left>0){
-        character.style.left = left - 2 + "px";    //left ball speed
-    }
-}
-function moveRight(){
-    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));   //move ball right side
-    if(left<380){
-        character.style.left = left + 2 + "px";     //ball goes frm position one to two
+        character.style.left = left - 2 + "px";   
     }
 }
 
-document.addEventListener("keydown", event => {     //click event
-    if(both==0){    //center
+//create a variable equal to the current right position of the character, and then set a new right position for the character 
+//adding 2 pixels. 
+function moveRight(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));   //move ball right side
+    if(left<380){
+        character.style.left = left + 2 + "px";    
+    }
+}
+
+// Create an event listener that runs whenever you press the left and right arrow keys. Inside create an interval that runs the 
+// right function depending which arrow key they pressed. 
+document.addEventListener("keydown", event => {     
+    if(both==0){    
         both++;
         if(event.key==="ArrowLeft"){
-            interval = setInterval(moveLeft, 1);        //ball speed
+            interval = setInterval(moveLeft, 1);       
         }
         if(event.key==="ArrowRight"){
             interval = setInterval(moveRight, 1);
         }
     }
 });
-document.addEventListener("keyup", event => {   //unpress key...stop ball from moving
+
+// Create another event listener, when you unpress any of the keys, clears the interval and stops the ball from moving. 
+document.addEventListener("keyup", event => {   
     clearInterval(interval);
     both=0;
 });
 
-var blocks = setInterval(function(){    //style for blocks and holes
+//style for blocks and holes. Create new HTML elements, Add some attributes to them, classes and IDs. 
+// Now append them to our game div. Create a variable “random”
+var blocks = setInterval(function(){    
     var blockLast = document.getElementById("block"+(counter-1));
     var holeLast = document.getElementById("hole"+(counter-1));
+
     if(counter>0){
         var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue("top"));
         var holeLastTop = parseInt(window.getComputedStyle(holeLast).getPropertyValue("top"));
     }
-    if(blockLastTop<400||counter==0){   //bottom gap increased    //counter used to make new block
+
+    if(blockLastTop<400||counter==0){  
         var block = document.createElement("div");
         var hole = document.createElement("div");
         block.setAttribute("class", "block");
         hole.setAttribute("class", "hole");
-        block.setAttribute("id", "block"+counter);  //
+        block.setAttribute("id", "block"+counter); 
         hole.setAttribute("id", "hole"+counter);
-        block.style.top = blockLastTop + 100 + "px";    //gap between two
+        block.style.top = blockLastTop + 100 + "px";   
         hole.style.top = holeLastTop + 100 + "px";
-        var random = Math.floor(Math.random() *360);       //
+        var random = Math.floor(Math.random() *360);      
         hole.style.left = random + "px";
-        game.appendChild(block);            //appende in game
+        game.appendChild(block);           
         game.appendChild(hole);
         currentBlocks.push(counter);
         counter++;
     }
+
+    // if the character top is above the top, then the game is over,  
+    // alert game over with the users score, and then refresh when they click okay.
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     var drop = 0;
     if(characterTop <= 0){
-        alert("Game over. Score: "+(counter-9));    //sir here if i removed 9 and pput another value then score shows incorrectly..why this?
+        alert("Game over. Score: "+(counter-9));    
         clearInterval(blocks);
         location.reload();
     }
+
+    // Created an array called current blocks, whenever we create a new block and hole, append the counter to the array. 
+    // ihole and iblock which are equal to the blocks and hole same as the current variable. 
+    // Then set new top positions for the block and hole , but then removing 0.5px from it.
 
     for(var i = 0; i < currentBlocks.length;i++){
         let current = currentBlocks[i];
@@ -72,20 +92,26 @@ var blocks = setInterval(function(){    //style for blocks and holes
         let ihole = document.getElementById("hole"+current);
         let iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
         let iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue("left"));
-        iblock.style.top = iblockTop - 0.5  + "px"; //block moving speed + block top lone
-        ihole.style.top = iblockTop - 0.5 + "px"; //block bottom line
-        if(iblockTop < -20){    //
+        iblock.style.top = iblockTop - 0.5  + "px"; 
+        ihole.style.top = iblockTop -0.5 + "px";
+
+        if(iblockTop < -20){   
             currentBlocks.shift();
             iblock.remove();
             ihole.remove();
         }
-        if(iblockTop-20<characterTop && iblockTop>characterTop){        //ball height from block
+
+        // first if - increments the drop variable if the character is currently on top of a block. 
+        // Second if - that will set drop back to 0 if you’re currently over a hole. 
+        if(iblockTop-20<characterTop && iblockTop>characterTop){       
             drop++;
             if(iholeLeft<=characterLeft && iholeLeft+30>=characterLeft){
                 drop = 0;
-            }
+            }       
         }
     }
+
+    // makes the ball fall if drop == 0 or makes it raise up if drop != 0. 
     if(drop==0){
         if(characterTop < 480){
             character.style.top = characterTop + 2 + "px";
